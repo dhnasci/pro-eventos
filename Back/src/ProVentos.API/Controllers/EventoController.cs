a7ed723b750740ae6dcc09e9e2e7013815022b17
dhnasci <dhnasci@hotmail.com>
2025-07-31 12:15:24 -0400

    Criação do evento e ajuste inicial na API

--- Back/src/ProVentos.API/Controllers/EventoController.cs
+++ Back/src/ProVentos.API/Controllers/EventoController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using ProVentos.API.Models;
 
 namespace ProVentos.API.Controllers
 {
@@ -11,15 +12,43 @@ namespace ProVentos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
+        public IEnumerable<Evento> _eventos = new Evento[] {
+            new Evento()
+            {
+                EventoId = 1,
+                Local = "São Paulo",
+                DataEvento = DateTime.Now.ToString("dd/MM/yyyy"),
+                Tema = "ProVentos 2023",
+                QtdPessaos = 1000,
+                ImagemURL = "https://www.example.com/imagem.jpg",
+                Lote = "1º Lote"
+            }, 
+            new Evento()
+            {
+                EventoId = 2,
+                Local = "Rio de Janeiro",
+                DataEvento = DateTime.Now.AddDays(30).ToString("dd/MM/yyyy"),
+                Tema = "ProVentos 2023 - Rio",
+                QtdPessaos = 500,
+                ImagemURL = "https://www.example.com/imagem2.jpg",
+                Lote = "2º Lote"
+            }
+            };
 
         public EventoController()
         {
         }
 
         [HttpGet]
-        public string Get()
+        public IEnumerable<Evento> Get()
         {
-            return "valor pelo Get";
+            return _eventos;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetById(int id)
+        {
+            return _eventos.Where(ev => ev.EventoId == id);
         }
 
         [HttpPost]
