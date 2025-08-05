4d30765a91aadde1662793b23a870b57642f0a07
dhnasci <dhnasci@hotmail.com>
2025-08-05 12:20:07 -0400

    Criação do DataContext com a primeira tabela

---
+++ Back/src/ProVentos.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProVentos.API.Models;
+
+namespace ProVentos.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Evento> Eventos { get; set; }
+    }
+}
