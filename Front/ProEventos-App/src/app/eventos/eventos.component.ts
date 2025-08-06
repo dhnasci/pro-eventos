010d73256199fa314efd29e3e658b092ec82874f
dhnasci <dhnasci@hotmail.com>
2025-08-06 08:47:21 -0400

    Criando função com array estatico

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -7,24 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any = [
-    {
-    Tema: 'Angular 11',
-    Local: 'Belo Horizonte'
-    },
-    {
-      Tema: '.NET 5',
-      Local: 'São Paulo'
-    },
-    {
-      Tema: 'Angular e suas novidades',
-      Local: 'Rio de Janeiro'
-    }
-  ];
+  public eventos: any;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.getEventos();
+  }
+
+  public getEventos(): void {
+    this.eventos = [
+      {
+        Tema: 'Angular 11',
+        Local: 'Belo Horizonte'
+      },
+      {
+        Tema: '.NET 5',
+        Local: 'São Paulo'
+      },
+      {
+        Tema: 'Angular e suas novidades',
+        Local: 'Rio de Janeiro'
+      }
+    ];
   }
 
 }
