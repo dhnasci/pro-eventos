46070fb3a6df9ef7f64b7547104ee1f164a114d4
dhnasci <dhnasci@hotmail.com>
2025-08-14 10:04:39 -0400

    ajuste do filtro do grid

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -9,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 export class EventosComponent implements OnInit {
 
   public eventos: any = [];
+  public eventosFiltrados: any = [];
   larguraImg = 150;
   margemImg = 2;
   isImgCollapsed = false;
+  // tslint:disable-next-line:variable-name
+  private _filtraLista = '';
+
+  public get filtraLista(): string {
+    return this._filtraLista;
+  }
+
+  public set filtraLista(valor: string) {
+    this._filtraLista = valor;
+    this.eventosFiltrados = (this.filtraLista) ? this.filtrarEventos(this.filtraLista) : this.eventos;
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -21,13 +33,23 @@ export class EventosComponent implements OnInit {
 
   public getEventos(): void {
     this.http.get('https://localhost:44355/api/Evento').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response;
+        this.eventosFiltrados = response;
+      },
       error => console.log(error)
     );
   }
 
   public alteraImagem(): void {
     this.isImgCollapsed = !this.isImgCollapsed;
+  }
+
+  private filtrarEventos(filtraPor: string): any {
+    filtraPor = filtraPor.toLowerCase();
+    return this.eventos.filter(
+      (evto: any) => evto.tema.toLowerCase().indexOf(filtraPor) !== -1
+    );
   }
 
 }
