3a3ecebcf661197c211f49ec7971bf5500c6b99b
dhnasci <dhnasci@hotmail.com>
2025-08-14 16:31:15 -0400

    Referenciando projeto

---
+++ Back/src/Proeventos.Domain/PalestranteEvento.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proeventos.Domain
+{
+    public class PalestranteEvento
+    {
+        public int PalestranteId { get; set; }
+        public Palestrante Palestrante { get; set; } // Assuming Palestrante is a class defined elsewhere in your project
+        public int EventoId { get; set; }
+        public Evento Evento { get; set; } // Assuming Evento is a class defined elsewhere in your project
+       
+    }
+}
---
+++ Back/src/Proeventos.Domain/RedeSocial.cs
@@ -0,0 +1,20 @@
+﻿using Proeventos.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proeventos.Domain
+{
+    public class RedeSocial
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string URL { get; set; }
+        public int EventoId { get; set; }
+        public Evento Evento { get; set; } // Assuming Evento is a class defined elsewhere in your project
+        public int PalestranteId { get; set; }
+        public Palestrante Palestrante { get; set; } // Assuming Propriedade is a class defined elsewhere in your project
+    }
+}
